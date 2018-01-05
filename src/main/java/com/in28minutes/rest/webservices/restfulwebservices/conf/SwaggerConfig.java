e8b2032b8be73ed8127b24439a639c1bc6e92a09
Antonio Escobar <aescobartizon@gmail.com>
2018-01-05 13:15:18 +0100

    Swagger customized

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/conf/SwaggerConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/conf/SwaggerConfig.java
@@ -1,8 +1,14 @@
 package com.in28minutes.rest.webservices.restfulwebservices.conf;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,10 +16,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+	private static final Contact DEFAULT_CONTACT = new Contact("Antonio Escobar", "", "");
+	private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Api Rest Documentation", "Api REST Documentation",
+			"2.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json","application/xml"));
 	
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO).produces(DEFAULT_PRODUCES_AND_CONSUMES).consumes(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
 
 }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/domain/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/domain/User.java
@@ -7,6 +7,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +16,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@ApiModel(description="All details about user.")
 public class User implements Serializable{
 
 	private static final long serialVersionUID = -5659121145198600763L;
@@ -22,10 +25,12 @@ public class User implements Serializable{
 	
 	@Size(min=2, message = "Name should have atleast 2 characters")
 	@NotNull
+	@ApiModelProperty(notes = "Lenght sould be unless 1")
 	private String name;
 	
 	@Past
 	@NotNull
+	@ApiModelProperty(notes = "Birth date should be in the past")
 	private Date birthDate;
 
 }
